8374085567a1a82b493f589aa2ab2618ab3f3953
mshtang <mshtang@outlook.com>
2019-05-03 22:48:44 +0200

    Restructure directory: organize models

---
+++ TrackerLibrary/Models/MatchupModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.Models
+{
+    public class MatchupModel
+    {
+        /// <summary>
+        /// The n-th Round in a tournament
+        /// </summary>
+        public int MatchupRound { get; set; }
+
+        /// <summary>
+        /// The winner in a matchup
+        /// </summary>
+        public TeamModel Winner { get; set; }
+
+        /// <summary>
+        /// The teams in a matchup
+        /// </summary>
+        public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+    }
+}
---
+++ TrackerLibrary/Models/PersonModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.Models
+{
+    public class PersonModel
+    {
+        /// <summary>
+        /// First name of a person
+        /// </summary>
+        public string FristName { get; set; }
+
+        /// <summary>
+        /// Last name of a person
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Email address of a person
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Cellphone number of a person
+        /// </summary>
+        public string CellphoneNumber { get; set; }
+    }
+}
